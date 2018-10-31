7debb0d699fb6cfa18a2b6dbcace5a03488b7934
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-10-31 08:48:56 +0100

    Update test_hello.sh

--- tests/test_hello.sh
+++ tests/test_hello.sh
@@ -4,6 +4,9 @@
 result=$(pwsh ./hello.ps1)
 
 #check if the output was the expected one
-if [ "$result" != "hello" ]; then
+if [ "$result" == "hello" ]; then
+	echo "GOOD: test passed"
+else
+	echo "BAD: test failed"
 	exit 1
 fi 
